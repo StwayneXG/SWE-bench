760bc8b665c3c466f4d7c9204d476f1609dfea9d
Irtaza Sajid <stwaynexg@gmail.com>
2025-05-09 01:20:36 +0500

    fixed by updating dataset from lite to verified

--- run_single.sh
+++ run_single.sh
@@ -4,4 +4,5 @@ python -m swebench.harness.run_evaluation \
     --predictions_path all_preds.jsonl \
     --max_workers 1 \
     --instance_ids django__django-12193 \
-    --run_id validate-mutation
+    --run_id validate-mutation \
+    --dataset_name princeton-nlp/SWE-bench_Verified
