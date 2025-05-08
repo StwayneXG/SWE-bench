c7f121a860eaa35a8912d527cc347968ee1b0108
Irtaza Sajid <stwaynexg@gmail.com>
2025-05-09 01:15:45 +0500

    update instance to run in run_single.sh

--- run_single.sh
+++ run_single.sh
@@ -3,5 +3,5 @@
 python -m swebench.harness.run_evaluation \
     --predictions_path all_preds.jsonl \
     --max_workers 1 \
-    --instance_ids django__django-10914 \
+    --instance_ids django__django-12193 \
     --run_id validate-mutation
