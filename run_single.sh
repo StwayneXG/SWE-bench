18ad718c7c9f1f7c008a889885c3cfba59b25436
Irtaza Sajid <stwaynexg@gmail.com>
2025-05-09 07:48:55 +0500

    add django-10914 to all_preds.jsonl

--- run_single.sh
+++ run_single.sh
@@ -3,6 +3,6 @@
 python -m swebench.harness.run_evaluation \
     --predictions_path all_preds.jsonl \
     --max_workers 1 \
-    --instance_ids django__django-12193 \
+    --instance_ids django__django-10914 \
     --run_id validate-mutation \
     --dataset_name princeton-nlp/SWE-bench_Verified
